b4d7b968fe805f454821e3a083b6c18001611f3a
Ábner Silva de Oliveira <abner.oliveira@serpro.gov.br>
2013-04-26 13:06:16 -0300

    alteração na folha de estilo. adição de thin.yaml

---
+++ test/unit/app_test.rb
@@ -0,0 +1,16 @@
+require 'rspec'
+
+# encoding: utf-8
+require 'mongoid'
+require 'mongoid/nested_serialization'
+require './kaneban.rb'
+
+# script para carga inicial de dados do projeto kanban. 
+# define projeto template, com colunas padronizadas para os kanbans
+Mongoid.load!('mongoid.yaml')
+
+class AppTest
+
+
+
+
